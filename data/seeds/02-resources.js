b2e2668e2fa5d4b394a2ddd4dfb9c6fd787224c6
Ryan Schueck <ryschueck@gmail.com>
2020-08-28 20:17:45 -0400

    Create: Setup resources table seed data

--- data/seeds/02-resources.js
+++ data/seeds/02-resources.js
@@ -1,8 +1,28 @@
-
-
 exports.seed = function(knex) {
-  return knex('recipes').insert([
-    {name: 'Crock Pot Roast'},
-    {name: 'Roasted Asparagus'},
+  return knex("resources").insert([
+    {
+      name: "Plate",
+      description:"food transporting device"
+    },
+    {
+       name: "fork",
+       description:"device to shovel food in face"
+     },
+     {
+       name: "knife",
+       description:"for cutting food"
+     },
+     {
+       name: "Napkin",
+       description:"for wiping food off face"
+     },
+     {
+       name: "shampoo",
+       description:"for washing hair"
+     },
+     {
+       name: "face wash",
+       description:"soap for washing face"
+     },
   ]);
 };
