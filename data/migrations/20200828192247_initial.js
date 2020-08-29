35cd3b2ff1cc88e9a408dd1cd59aac0961c0908a
Ryan Schueck <ryschueck@gmail.com>
2020-08-28 20:02:48 -0400

    Fixed: Fixed typo's in Migration file creates database correctly now

--- data/migrations/20200828192247_initial.js
+++ data/migrations/20200828192247_initial.js
@@ -1,53 +1,54 @@
 
-exports.up = async function(knex) {
-  await knex.scheme
-    .createTable("projcts", tbl => {
-        tbl.increments("id")
-        tbl.text("name", 128).notNull().unique()
-        tbl.text("description", 500)
-        tbl.boolean("completed").defaultTo(false)
-    })
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("projcts", tbl => {
+            tbl.increments("id")
+            tbl.text("name", 128).notNull().unique()
+            tbl.text("description", 500)
+            tbl.boolean("completed").defaultTo(false)
+        })
 
-    .createTable("resource", tbl => {
-        tbl.increments("id")
-        tbl.text("name", 128).notNull()
-        tbl.text("description")
-    })
+        .createTable("resource", tbl => {
+            tbl.increments("id")
+            tbl.text("name", 128).notNull()
+            tbl.text("description")
+        })
 
-    .createTable("tasks", tbl => {
-        tbl.increments("id")
-        tbl.text("description").notNull()
-        tbl.text("notes")
-        tbl.boolean("completed").defaultTo(false)
+        .createTable("tasks", tbl => {
+            tbl.increments("id")
+            tbl.text("description").notNull()
+            tbl.text("notes")
+            tbl.boolean("completed").defaultTo(false)
 
-        tbl.ineger("projects_id")
-        .notNull()
-        .references("projects.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCAED")
-    })
+            tbl.integer("projects_id")
+                .notNull()
+                .references("projects.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE")
+        })
 
-    .createTable("project_task", tbl =>{
-        tbl.primary(["projects_id", "resource_id"])
+        .createTable("project_task", tbl => {
+            tbl.primary(["projects_id", "resource_id"])
 
-        tbl.ineger("projects_id")
-        .notNull()
-        .references("projects.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCAED")
+            tbl.integer("projects_id")
+                .notNull()
+                .references("projects.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE")
 
-        tbl.ineger("resource_id")
-        .notNull()
-        .references("resource.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCAED")
+            tbl.integer("resource_id")
+                .notNull()
+                .references("resource.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE")
 
-    })
+        })
 };
 
-exports.down = async function(knex) {
-    await knex.schema.dropTableIfExists("project_task")
-    await knex.schema.dropTableIfExists("tasks")
-    await knex.schema.dropTableIfExists("resource")
-    await knex.schema.dropTableIfExists("projcts")
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists("project_task")
+        .dropTableIfExists("tasks")
+        .dropTableIfExists("resource")
+        .dropTableIfExists("projcts")
 };
